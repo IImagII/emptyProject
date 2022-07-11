9a670844d610d4a68b7acdba6ab916ea7601af47
iimagii <mag_1@ukr.net>
2022-07-11 20:17:00 +0300

    reduce 2

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-let array = ['apple', 'banana', 'peach', 'orange'];
-let total = array.reduce((acc, elem) => {
-   if (acc[elem]) {
-      acc[elem] += 1;
-   } else {
-      acc[elem] = 1;
+let input = [
+   [1, 3],
+   [2, 6],
+   [8, 10],
+   [15, 18],
+];
+let input2 = [
+   [1, 4],
+   [4, 5],
+];
+
+function merge(intervals) {
+   if (intervals.length < 2) return intervals;
+   intervals.sort((a, b) => a[0] - b[0]);
+   let res = [intervals[0]];
+   for (let interval of intervals) {
+      let recent = res[res.length - 1];
+      if (recent[1] >= interval[0]) {
+         recent[1] = Math.max(recent[1], interval[1]);
+      } else {
+         res.push(interval);
+      }
    }
+   return res;
+}
 
-   return acc;
-}, {});
-
-console.log(total);
+console.log(merge(input));
+console.log(merge(input2));
